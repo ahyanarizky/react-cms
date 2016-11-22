0a12e8ad74cc02e1a22ce087a682de4a0da87ffd
Ken Duigraha Putra <kenduigraha@yahoo.com>
2016-11-23 06:49:00 +0700

    login auth

--- client/script/indexComponents.js
+++ client/script/indexComponents.js
@@ -1,40 +1,59 @@
 const Router = ReactRouter.Router
 const Route = ReactRouter.Route
 const Link = ReactRouter.Link
-const browserHistory = ReactRouter.browserHistory
-/*
-  <!-- navbar -->
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">cms-api</a>
+const hashHistory = ReactRouter.hashHistory
+const withRouter = ReactRouter.withRouter
+
+function getUser(){
+  var token = localStorage.getItem('token')
+  if (!token) return
+  else {
+    return(
+      jwt_decode(token)
+    )
+  }
+}
+
+const Navigation = React.createClass({
+  render(){
+    return(
+      !getUser() ?
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <Link className="navbar-brand" to="/">cms-api</Link>
         </div>
-        <ul class="nav navbar-nav">
-          <li><a href="login.html">Login</a></li>
-          <li><a href="register.html">Register</a></li>
+        <ul className="nav navbar-nav">
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Register</Link></li>
         </ul>
       </div>
-    </nav>
-  <!-- navbar -->
-  <div class="container">
-    <h1>Welcome to cms-api, please login or register</h1>
-  </div>
-*/
-const Navbar = React.createClass({
+      :
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <Link className="navbar-brand" to="/dashboard">cms-api</Link>
+        </div>
+        <ul className="nav navbar-nav">
+          <li><Link to="/data">Data</Link></li>
+          <li><Link to="/dataDate">Data Date</Link></li>
+          <li><Link to="/logout">Logout</Link></li>
+        </ul>
+      </div>
+    )
+  }
+})
+
+const App = React.createClass({
   render: function(){
     return(
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <Link className="navbar-brand" to="/">cms-api</Link>
-          </div>
-          <ul className="nav navbar-nav">
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-          </ul>
-        </div>
-        {this.props.children}
-      </nav>
+      <div>
+        <nav className="navbar navbar-default">
+          <Navigation />
+        </nav>
+        {(hashHistory.getCurrentLocation().pathname === '/')
+          ?
+            (!getUser() ? <Home />  : <Dashboard />)
+          : this.props.children}
+      </div>
     )
   }
 })
@@ -43,7 +62,6 @@ const Home = React.createClass({
   render: function(){
     return(
       <div>
-        <Navbar />
         <div className="container">
           <h1>Welcome to cms-api, please login or register</h1>
         </div>
@@ -52,24 +70,92 @@ const Home = React.createClass({
   }
 })
 
-const Login = React.createClass({
+const Dashboard = React.createClass({
   render: function(){
     return(
       <div>
-        <Navbar />
-          <div className="container">
+        <div className="container">
+          <h1>Dashboard</h1>
+        </div>
+      </div>
+    )
+  }
+})
+
+const Login = React.createClass({
+  getInitialState(){
+    return({
+      error: false,
+      username: '',
+      password: ''
+    })
+  },
+  handleUsername(e){
+    this.setState({
+      username: e.target.value
+    })
+  },
+  handlePassword(e){
+    this.setState({
+      password: e.target.value
+    })
+  },
+  handleSubmitLogin(e){
+    e.preventDefault()
+    var data_login = {
+      username: this.state.username.trim(),
+      password: this.state.password.trim()
+    }
+
+    if(!data_login.username || !data_login.password){
+      return
+    }else{
+      $.post({
+        url: "http://localhost:3000/api/users/login",
+        data: data_login,
+        success: function(login_user){
+          localStorage.setItem('token', login_user.token)
+          // this.setState({
+          //   token: login_user.token
+          // })
+          if(getUser().username){
+            this.props.router.replace('/dashboard')
+          }else{
+            alert('input is wrong')
+            this.setState({
+              error: true,
+              token: ''
+            })
+            this.props.router.replace('/login')
+          }
+        }.bind(this)
+      })
+
+      this.setState({
+        username: '',
+        password: ''
+      })
+    }
+  },
+  render: function(){
+    return(
+      <div>
+          <div className="container" onSubmit={this.handleSubmitLogin}>
             <form id="form_login">
               <div className="form-group">
                 <label htmlFor="username">Username</label>
-                <input type="text" className="form-control" id="username" />
+                <input type="text" className="form-control" id="username" onChange={this.handleUsername} />
               </div>
               <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password" />
+                <input type="password" className="form-control" id="password" onChange={this.handlePassword} />
               </div>
               <div className="form-group">
-                <button type="button" id="btn_login" className="btn btn-lg btn-success">Login</button>
+                <button type="submit" id="btn_login" className="btn btn-lg btn-success">Login</button>
               </div>
+              {this.state.error && (
+                <p>Input is wrong</p>
+              )}
             </form>
           </div>
       </div>
@@ -81,7 +167,6 @@ const Register = React.createClass({
   render: function(){
     return(
       <div>
-        <Navbar />
           <div className="container">
             <form id="form_register">
               <div className="form-group">
@@ -110,12 +195,32 @@ const Register = React.createClass({
   }
 })
 
+function authDashboard(nextState, replace){
+  if(!localStorage.getItem('token')){
+    replace({
+      pathname: '/login',
+      // state: { nextPathname: nextState.location.pathname }
+    })
+  }
+}
+
+function processLogout(nextState, replace){
+  localStorage.removeItem('token')
+  replace({
+    pathname: '/login',
+    // state: { nextPathname: nextState.location.pathname }
+  })
+}
+
 ReactDOM.render(
   (
-    <Router history={browserHistory}>
-      <Route path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <Route path="/login" component={!getUser() ? Login : Dashboard} />
+        <Route path="/register" component={!getUser() ? Register : Dashboard} />
+        <Route path="/dashboard" component={Dashboard} onEnter={authDashboard} />
+        <Route path="/logout" onEnter={processLogout} />
+      </Route>
     </Router>
   ), document.getElementById('content')
 )
